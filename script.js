0753acb86fafa67201af485ce72d652b3e72b080
rinaAks <128968330+rinaAks@users.noreply.github.com>
2026-02-25 15:43:22 +0300

    new design

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function showProject(projectNumber) {
     });
     document.getElementById(`project${projectNumber}`).style.display = 'block';
 }
+
+// 1. Находим ВСЕ кнопки с этим классом
+const buttons = document.querySelectorAll('.toggle-btn');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // 2. Находим именно тот блок, который идет сразу ПОСЛЕ нажатой кнопки
+    const content = btn.nextElementSibling;
+    
+    // 3. Переключаем класс только у этого блока
+    content.classList.toggle('open');
+  });
+});
